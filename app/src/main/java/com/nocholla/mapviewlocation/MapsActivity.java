6bd58ade05b605f182b9f14154a7738bbce37135
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2018-12-02 16:50:45 -0800

    Updated San Diego, Coordinates

--- app/src/main/java/com/nocholla/mapviewlocation/MapsActivity.java
+++ app/src/main/java/com/nocholla/mapviewlocation/MapsActivity.java
@@ -29,7 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near San Diego, USA.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -38,10 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in San Diego and move the camera
+        LatLng sanDiego = new LatLng(32.715736, -117.161087);
+        mMap.addMarker(new MarkerOptions().position(sanDiego).title("Marker in San Diego"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sanDiego));
     }
 
 }
